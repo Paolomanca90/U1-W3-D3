a0379cbe352f1c9dbd65b48b91b2d3bd0f97161f
Paolomanca90 <paolomanca1990@gmail.com>
2023-06-07 16:46:18 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const taskForm = document.getElementById('my-form')
+taskForm.addEventListener('submit', function(e) {
+    e.preventDefault()
+    const newTask = document.getElementById('inputField')
+    const myTask = newTask.value
+
+    let taskCard = document.createElement('div')
+    taskCard.classList.add('task-card')
+    taskCard.innerHTML = `
+        <p>${myTask}</p>
+        <input type='checkbox'/> 
+        <button>Elimina task</button>
+    `
+    const savedTask = document.getElementById('new-task')
+    savedTask.appendChild(taskCard)
+
+    const check = document.querySelectorAll('.task-card input')
+    check.forEach((c) =>{
+        c.addEventListener('click', function(e){
+           
+            
+        })
+    })
+
+    const buttons = document.querySelectorAll('.task-card button')
+    buttons.forEach((b) =>{
+        b.addEventListener('click', function(e) {
+            let buttonClicked = e.target
+            let taskToRemove = buttonClicked.parentElement
+            taskToRemove.remove()
+        })
+    })
+
+    newTask.value = ''
+})
